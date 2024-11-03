aa17602b63a38e5568ade60ade68ea02cf4d5c11
Isaac Zakaria <isaac.zkr@gmail.com>
2024-11-03 13:56:25 +0900

    Add class Interp to extra

--- meitner/extra.py
+++ meitner/extra.py
@@ -1,4 +1,9 @@
+import numpy as np
+import pandas as pd
+
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+from scipy.interpolate import UnivariateSpline # default is cubic spline
+
 
 class Aux:
     '''
@@ -67,3 +72,58 @@ class Aux:
             ax.xaxis.set_major_locator(MultipleLocator(major_tick_multiple))
         if minor_tick_multiple > 0:
             ax.xaxis.set_minor_locator(MultipleLocator(minor_tick_multiple))
+
+
+class Interp:
+    @staticmethod
+    def interpolate_df(df, column, bounds, step):
+        """
+        Use splines to reevaluate a DataFrame on a specified grid for a given column.
+        
+        Parameters:
+        df (pd.DataFrame): The DataFrame to reevaluate.
+        column (str): The column name to base the reevaluation on.
+        grid_points (array-like): The grid points to reevaluate the DataFrame on.
+        
+        Returns:
+        pd.DataFrame: The reevaluated DataFrame.
+        """
+        grid_points = np.arange(bounds[0], bounds[1], step)
+        # Sort the DataFrame based on the specified column
+        df_sorted = df.sort_values(by=column)
+        
+        # Create a dictionary to store the reevaluated data
+        reevaluated_data = {column: grid_points}
+        
+        # Use splines to reevaluate each column in the DataFrame
+        for col in df_sorted.columns:
+            if col != column:
+                spline = UnivariateSpline(df_sorted[column], df_sorted[col], s=0)
+                reevaluated_data[col] = spline(grid_points)
+        
+        # Create a new DataFrame with the reevaluated data
+        df_reevaluated = pd.DataFrame(reevaluated_data)
+        
+        return df_reevaluated
+
+    @classmethod
+    def interpolate_dfs(cls, dfs, bounds, column='B.E.', step=0.1):
+        data_interpolated = []
+        for df in dfs:
+            df_interpolated = cls.interpolate_df(df, column, bounds, step)
+            data_interpolated.append(df_interpolated)
+        return data_interpolated
+
+    @staticmethod
+    def avg_dfs(dfs):
+        df_avg = dfs[0].copy()
+        for df in dfs[1:]:
+            df_avg += df
+        df_avg /= len(dfs)
+        return df_avg
+
+    @classmethod
+    def interpolate_avg_dfs(cls, dfs, bounds, column='B.E.', step=0.1):
+        data_interpolated = cls.interpolate_dfs(dfs, bounds, column=column, step=step)
+        df_avg = cls.avg_dfs(data_interpolated)
+        return data_interpolated, df_avg
